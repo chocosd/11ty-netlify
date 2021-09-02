921dc0de9c4c468dd954bc6f3445464f24c9b6cc
Steven Dix <hello@stevendix.co.uk>
2021-09-03 00:00:48 +0100

    task(clean-up): add gitignore file

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,4 @@ module.exports = (eleventyConfig) => {
     eleventyConfig.addPassthroughCopy("images")
     eleventyConfig.addPassthroughCopy("admin")
     eleventyConfig.addPassthroughCopy("css")
-    eleventyConfig.setUseGitIgnore(true);
 }
