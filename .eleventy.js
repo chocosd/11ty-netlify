4832850ad09d4c30f9777a69b39cb750763e6160
Steven Dix <hello@stevendix.co.uk>
2021-09-04 01:24:42 +0100

    begin styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,10 @@
+const { DateTime } = require("luxon")
+
 module.exports = (eleventyConfig) => {
     eleventyConfig.addPassthroughCopy("images")
     eleventyConfig.addPassthroughCopy("admin")
     eleventyConfig.addPassthroughCopy("css")
+
+    eleventyConfig.addFilter("postDate", (dateObj) => DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED))
+
 }
--- heroes/first-hero.md
+++ heroes/first-hero.md
@@ -1,8 +1,8 @@
 ---
-layout: layouts/hero.njk
+layout: heroblogs.njk
 title: Welcome to the blog
 subtitle: A work in progress and updatable through netlify CMS
 date: 2020-10-08
-featuredImage: /images/uploads/download.png
+featuredImage: /images/uploads/azure-bay.jpeg
 ---
 
